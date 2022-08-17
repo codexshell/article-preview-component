8f2d2b47434d819597791bd186875df332483050
silvanos <ericoderito@gmail.com>
2022-08-17 12:18:17 +0300

    extend tailwind classes
    use the styles provided in the style-guide.md
    add a lib folder for components and modules

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<svelte:head>
+  <title>Frontend Mentor | Article preview component</title>
+</svelte:head>
+
+<section>
+</section>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+				'desaturated-dark-blue': 'hsl(214, 17%, 51%)',
+				'grayish-blue': 'hsl(212, 23%, 69%)',
+				'light-grayish-blue': 'hsl(210, 46%, 95%)'
+			},
+			fontSize: {
+				'body-copy': '13px'
+			},
+			fontFamily: {
+				heading: ['Manrope', 'sans-serif']
+			}
+		}
+	},
+	plugins: []
+};
